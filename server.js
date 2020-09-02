b28ca5e2e5d19bba2a22870cc3deeca3e9767beb
No-exceptions <8astard8word@gmail.com>
2020-09-02 17:47:52 +0300

    j

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 
@@ -12,10 +14,21 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.route("/").get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'})
   //Change the response to render the Pug template
 });
+
+
+app.use(session({
+  secret: "lkdjal3",
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
