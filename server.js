f1adf48a579c3c042953c0bf0634496f208a770f
No-exceptions <8astard8word@gmail.com>
2020-09-02 16:56:37 +0300

    j

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const app = express();
 
 app.set('view engine', 'pug')
-app.set('views', './views/pug')
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -14,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
-  res.render("index")
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'})
   //Change the response to render the Pug template
 });
 
