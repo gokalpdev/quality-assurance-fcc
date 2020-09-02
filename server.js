eade50338811784fecda84a92999ddbe97a6774f
No-exceptions <8astard8word@gmail.com>
2020-09-02 15:44:12 +0300

    ex1

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
+
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
+  res.render("index")
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
 });
 
 app.listen(process.env.PORT || 3000, () => {
