000eb708c5401b520e3381eac1206c2fe410b46d
gokalpdev <gokalpdev.info@gmail.com>
2020-09-04 00:26:50 +0300

    j

--- server.js
+++ server.js
@@ -63,21 +63,22 @@ mongo.connect(process.env.DATABASE, (err, client) => {
       );
     });
 
-    app.post('/login', 
-      passport.authenticate('local',  {failureRedirect: '/' }), 
-      (req, res) => {
-        res.redirect('/profile')
-    })
-
-    app.get('/profile', (req, res)=>{
-      res.render(process.cwd() + '/views/pug/profile')
-    })
-
     app.route("/")
-      .get((req, res) => {
-        res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login', showLogin: true, showRegistration: true})
-      //Change the response to render the Pug template
+    .get((req, res) => {
+      res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login', showLogin: true, showRegistration: true})
+    //Change the response to render the Pug template
+  });
+
+    app.route('/login')
+    .post(passport.authenticate('local', { failureRedirect: '/' }),(req,res) => {
+        res.redirect('/profile');
     });
+
+  app.route('/profile')
+    .get((req,res) => {
+        res.render(process.cwd() + '/views/pug/profile');
+    });
+
 
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
