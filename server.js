81e268e00a4b0f853b13ec4682c49ef270ce7f4f
gokalpdev <gokalpdev.info@gmail.com>
2020-09-04 02:22:44 +0300

    j

--- server.js
+++ server.js
@@ -92,6 +92,36 @@ mongo.connect(process.env.DATABASE, (err, client) => {
         res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
     });
 
+    app.route('/register')
+  .post((req, res, next) => {
+    db.collection('users').findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        db.collection('users').insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, user);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+
     app.use((req, res, next) => {
       res.status(404)
         .type('text')
